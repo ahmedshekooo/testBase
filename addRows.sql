73b55626e4ca6a5f5b7372e96264deede8d72e2d
ahmed <ahmedshekooo2010@gmail.com>
2025-03-08 17:55:14 +0200

    add new rows

---
+++ addRows.sql
@@ -0,0 +1,54 @@
+INSERT INTO jobs(
+    job_id, job_title, min_salary, max_salary,still_working
+)
+VALUES(
+    '11', 'Programmer', 10000, 100000, true
+),
+(
+    '12', 'Designer', 10000, 100000, false
+),
+(
+    '13', 'Manager', 10000, 100000, true
+),
+(
+    '14', 'Director', 10000, 100000, true
+),
+(
+    '15', 'CEO', 10000, 100000, true
+),
+(
+    '16', 'CFO', 10000, 100000, true
+),
+(
+    '17', 'CTO', 10000, 100000, true
+),
+(
+    '18', 'COO', 10000, 100000, true
+),
+(
+    '19', 'CIO', 10000, 100000, false
+),
+(
+    '20', 'CISO', 10000, 100000, false
+),
+(
+    '21', 'CPO', 10000, 100000, false
+),
+(
+    '22', 'CLO', 10000, 100000, true
+),
+(
+    '23', 'CDO', 10000, 100000, true
+),
+(
+    '24', 'CBO', 10000, 100000, true
+),
+(
+    '25', 'CVO', 10000, 100000, true
+),
+(
+    '26', 'CFO', 10000, 100000, false
+),
+(
+    '27', 'CFO', 10000, 100000, false
+);
